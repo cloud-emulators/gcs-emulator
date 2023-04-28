a7330d87b22cb1111c6b235d8f22efd8460cfdd9
Mahmoud Bahaa <mahmoud.bahaa.eg@gmail.com>
2023-04-28 12:41:21 +0300

    fix go test cases

--- go-client/bucket_test.go
+++ go-client/bucket_test.go
@@ -24,51 +24,102 @@
 package main
 
 import (
-	"context"
-	"google.golang.org/api/option"
-	"testing"
-	"time"
-
 	"cloud.google.com/go/storage"
+	"context"
+	"google.golang.org/api/googleapi"
+	"google.golang.org/api/option"
+	"strings"
+	"testing"
 )
 
 var (
-	projectId = "project-id"
-	BUCKET1   = "bucket1"
+	ProjectId     = "project-id"
+	Bucket1       = "bucket1"
+	InvalidBucket = "****bucket****"
 	//BUCKET2   = "bucket2"
 )
 
-//func tempDir() string {
-//	if //goland:noinspection GoBoolExpressions
-//	runtime.GOOS == "linux" {
-//		return "/var/tmp"
-//	} else {
-//		return os.TempDir()
-//	}
-//}
+var ctx context.Context
+var client *storage.Client
+var bucket1 *storage.BucketHandle
 
-func TestServerClientBucketAlreadyExists(t *testing.T) {
-	ctx := context.Background()
+func setup(t *testing.T) func(t *testing.T) {
+	ctx = context.Background()
 	endpoint := option.WithEndpoint("http://localhost:8080/storage/v1/")
-	client, err := storage.NewClient(ctx, endpoint)
+	noAuth := option.WithoutAuthentication()
+	var err error
+	client, err = storage.NewClient(ctx, endpoint, noAuth)
 	if err != nil {
 		t.Errorf("storage.NewClient: %v", err)
 	}
-	defer func(client *storage.Client) {
-		err := client.Close()
-		if err != nil {
-			t.Errorf("Error while closing client: %v", err)
-		}
-	}(client)
 
-	ctx, cancel := context.WithTimeout(ctx, time.Second*30)
-	defer cancel()
+	bucket1 = client.Bucket(Bucket1)
+	_ = bucket1.Delete(ctx)
+	return func(t *testing.T) {
+		func(client *storage.Client) {
+			err := client.Close()
+			if err != nil {
+				t.Errorf("Error while closing client: %v", err)
+			}
+		}(client)
+	}
+}
 
-	bucket := client.Bucket(BUCKET1)
-	_ = bucket.Delete(ctx)
-	if err := bucket.Create(ctx, projectId, &storage.BucketAttrs{}); err != nil {
-		t.Errorf("Bucket(%q).Create: %v", BUCKET1, err)
+func TestBucketValid(t *testing.T) {
+	defer setup(t)(t)
+	if err := bucket1.Create(ctx, ProjectId, &storage.BucketAttrs{}); err != nil {
+		t.Errorf("Bucket(%q).Create: %v", Bucket1, err)
 	}
 
-	t.Logf("Created bucket %v.\n", BUCKET1)
+	t.Logf("Created bucket %v.\n", Bucket1)
+}
+
+func TestBucketInValid(t *testing.T) {
+	defer setup(t)(t)
+	if err := client.Bucket(InvalidBucket).Create(ctx, ProjectId, &storage.BucketAttrs{}); err != nil {
+		t.Logf("Bucket(%q).Create: %v", InvalidBucket, err)
+		gErr, ok := err.(*googleapi.Error)
+		if !ok {
+			t.Errorf("Incorrect Error type thrown: %v", err)
+		}
+
+		if gErr.Code != 400 {
+			t.Errorf("Incorrect status code returned, expected: 400 but got: %q", gErr.Code)
+		}
+
+		if !strings.HasPrefix(gErr.Message, "Invalid bucket name") {
+			t.Errorf("Incorrect error message returned: %q", gErr.Message)
+		}
+
+		return
+	}
+
+	t.Errorf("Should throw an error when bucket name is invalid")
+}
+func TestBucketAlreadyExists(t *testing.T) {
+	defer setup(t)(t)
+	if err := bucket1.Create(ctx, ProjectId, &storage.BucketAttrs{}); err != nil {
+		t.Errorf("Bucket(%q).Create: %v", Bucket1, err)
+	}
+
+	t.Logf("Created bucket %v.\n", Bucket1)
+
+	if err := bucket1.Create(ctx, ProjectId, &storage.BucketAttrs{}); err != nil {
+		gErr, ok := err.(*googleapi.Error)
+		if !ok {
+			t.Errorf("Incorrect Error type thrown: %v", err)
+		}
+
+		if gErr.Code != 409 {
+			t.Errorf("Incorrect status code returned, expected: 409 but got: %q", gErr.Code)
+		}
+
+		if gErr.Message != "Your previous request to create the named bucket succeeded and you already own it." {
+			t.Errorf("Incorrect error message returned: %q", gErr.Message)
+		}
+
+		return
+	}
+
+	t.Errorf("Should throw an error when bucket name already exists")
 }
